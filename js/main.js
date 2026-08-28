21de6d4bfee8fc11488f87e9d2fa96771acb7a1d
Hoàng Vân Anh <vananhhha02@gmail.com>
2026-08-28 21:05:55 +0700

    Add FAQ accordion and dark mode

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 import { initNav, initHeaderOnScroll, initToTop } from "./nav.js";
+import { initFaq } from "./faq.js";
+import { initTheme } from "./theme.js";
 
 initNav();
 initHeaderOnScroll();
 initToTop();
+initFaq();
+initTheme();
