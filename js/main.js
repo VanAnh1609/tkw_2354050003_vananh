29268e4556a8d73127f8bcd1bd2ed038cd9d3275
Hoàng Vân Anh <vananhhha02@gmail.com>
2026-08-28 21:34:42 +0700

    Add pricing toggle and reveal animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 import { initNav, initHeaderOnScroll, initToTop } from "./nav.js";
 import { initFaq } from "./faq.js";
 import { initTheme } from "./theme.js";
+import { initPricing } from "./pricing.js";
+import { initReveal } from "./reveal.js";
 
 initNav();
 initHeaderOnScroll();
 initToTop();
 initFaq();
 initTheme();
+initPricing();
+initReveal();
